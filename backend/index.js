50b5896206eac4a549dcd7b642b64080b05906de
MujahidEmon <mujahidulislam.official@gmail.com>
2026-08-28 22:56:38 +0600

    added all filter filter in getall api

--- backend/index.js
+++ backend/index.js
@@ -19,60 +19,129 @@ app.get("/", (req, res) => {
   res.send("srd server is running");
 });
 
-
 async function startServer() {
   try {
     await dbConnect();
     const reqCollection = await getDatabase().collection("requests");
 
-
-
-
-
     // get all requests
     app.get("/api/requests", async (req, res) => {
       try {
-        const {search, status, priority, category, assignedPerson} = req.query;
-        const filter = {}
+        const { search, status, priority, category, assignedPerson } =
+          req.query;
+        const filter = {};
 
-        if(search){
-            const searchText = search.trim();
+        // search filter
+        if (search) {
+          const searchText = search.trim();
 
-            if(searchText){
-                filter.$or = [
-                    {
-                        title: {$regex: searchText,
-                            $options:"i"
-                        }
-                    },
-                    {
-                        description: {$regex: searchText, $options: "i"},
-                        
-                    },
-                    {
-                        requesterName: {$regex: searchText,
-                            $options: "i"
-                        }
-                    }
-                ]
-            }
+          if (searchText) {
+            filter.$or = [
+              {
+                title: { $regex: searchText, $options: "i" },
+              },
+              {
+                description: { $regex: searchText, $options: "i" },
+              },
+              {
+                requesterName: { $regex: searchText, $options: "i" },
+              },
+            ];
+          }
         }
-        
-        const requests = await reqCollection.find(filter).toArray();
+
+        //filter by status
+        if (status) {
+          const statuses = [
+            "Open",
+            "In Progress",
+            "Waiting for User",
+            "Resolved",
+            "Closed",
+          ];
+
+          if (!statuses.includes(status)) {
+            return res.send(400).json({
+              success: false,
+              message: "invalid Status",
+            });
+          }
+
+          filter.status = status;
+        }
+
+        // filter by priority
+        if (priority) {
+          const priorrities = ["Low", "Medium", "High", "Urgent"];
+          if (!priorrities.includes(priority)) {
+            return res.status(400).json({
+              success: false,
+              message: "Invalid priority",
+            });
+          }
+          filter.priority = priority;
+        }
+
+        // filter by category
+        if (category) {
+          const categories = [
+            "Hardware",
+            "Software",
+            "Access",
+            "Network",
+            "Other",
+          ];
+
+          if (!categories.includes(category)) {
+            return res.status(400).json({
+              success: false,
+              message: "Invalid category",
+            });
+          }
+          filter.category = category;
+        }
+
+        // filte by assigned person
+        if (assignedPerson) {
+          if (assignedPerson === "unassigned") {
+            filter.assignedPerson = null;
+          } else {
+            const supportPeople = [
+              { id: "hasan", name: "Hasan Mahmud" },
+              { id: "nusrat", name: "Nusrat Jahan" },
+              { id: "raihan", name: "Raihan Ahmed" },
+              { id: "sadia", name: "Sadia Khan" },
+            ];
+
+            const person = supportPeople.find(
+              (person) => person.id === assignedPerson,
+            );
+
+            if (!person) {
+              return res.status(400).json({
+                success: false,
+                message: "Unknown support person",
+              });
+            }
+            filter["assignedPerson.id"] = assignedPerson;
+          }
+        }
+
+        // get req
+
+        const requests = await reqCollection
+          .find(filter)
+          .sort({ updatedAt: -1 })
+          .toArray();
 
         res.status(200).send(requests);
-
       } catch (error) {
-        
+        res.status(500).json({
+          success: false,
+          message: "Failed to get requests",
+        });
       }
     });
-
-
-
-
-
-
-
 
     app.listen(port, () => {
       console.log(`Server running at http://localhost:${port}`);
