04421034d541f2ba711be1a596ece9d6110e224d
MujahidEmon <mujahidulislam.official@gmail.com>
2026-08-30 12:10:59 +0600

    testing ongoing

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -7,12 +7,12 @@ import Link from 'next/link';
 
 const Homepage = () => {
     return (
-        <div className='max-w-7xl min-h-screen mx-auto p-4 flex flex-col items-center justify-center md:gap-5 gap-4'>
+        <div className='max-w-7xl min-h-screen mx-auto flex flex-col items-center justify-center md:gap-5 gap-4'>
             <h1 className='font-bold lg:text-5xl text-primary/90 md:text-4xl text-3xl text-center'><span className='lg:text-4xl md:text-3xl text-lg text-black font-semibold'>Welcome to</span> <br /> Service Request Desk!</h1>
             <p className='text-center text-black/70'>Choose how you would like to proceed</p>
 
             <div className='flex flex-col md:flex-row items-center justify-center gap-4'>
-                <div className="card border border-primary/40 bg-base-100 w-96 shadow-sm">
+                <div className="card border border-primary/40 bg-base-100 w-full shadow-sm">
                     <figure className="px-10 pt-10">
                         <Image
                             src={requesterIcon}
@@ -32,7 +32,7 @@ const Homepage = () => {
                     </div>
                 </div>
 
-                <div className="card border border-primary/40 bg-base-100 w-96 shadow-sm">
+                <div className="card border border-primary/40 bg-base-100 w-full shadow-sm">
                     <figure className="px-10 pt-10">
                         <Image
                             src={supportIcon}
